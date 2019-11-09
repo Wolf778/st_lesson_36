d47efd8a7167284cf23491ffeb0aded7ec889875
Wolf778 <7473747@mail.ru>
2019-11-09 21:27:27 +0300

    Внесены исправления в assert

--- test/test_items.py
+++ test/test_items.py
@@ -3,10 +3,12 @@ import time
 def test_guest_should_see_login_link(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     browser.get(link)
-    time.sleep(10)
-    btn = browser.find_element_by_css_selector("button.btn-add-to-basket")
-    assert len(btn.text) > 0, 'Buttom not find...'
     time.sleep(5)
+    btn = browser.find_elements_by_class_name("vbtn-add-to-basket")
+    assert len(btn) > 0, 'Buttom not find...'
+    time.sleep(3)
+
+
 
 
 
